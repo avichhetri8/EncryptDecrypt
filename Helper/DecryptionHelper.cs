2ff45b60d7bc4760ad96a86d537563541cf49abe
Avi Chhetri <Avi Chhetri@DESKTOP-1KGM680>
2024-06-14 20:22:28 -0400

    decryption helper

---
+++ Helper/DecryptionHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+
+namespace Helper
+{
+    public class DecryptionHelper
+    {
+        public static string Decrypt<T>(string strToDecrypt, byte[] secretKey, byte[] iv, CipherMode mode, PaddingMode padding) where T : SymmetricAlgorithm, new()
+        {
+            byte[] buffer = Convert.FromBase64String(strToDecrypt);
+
+            using (T algorithm = new T())
+            {
+                algorithm.Key = secretKey;
+                algorithm.IV = iv;
+                algorithm.Mode = mode;
+                algorithm.Padding = padding;
+
+                ICryptoTransform decryptor = algorithm.CreateDecryptor(algorithm.Key, algorithm.IV);
+
+                using MemoryStream memoryStream = new(buffer);
+                using CryptoStream cryptoStream = new(memoryStream, decryptor, CryptoStreamMode.Read);
+                using StreamReader streamReader = new(cryptoStream);
+                return streamReader.ReadToEnd();
+            }
+        }
+    }
+}
