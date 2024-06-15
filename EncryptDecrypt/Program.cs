166a6cf217da3af3276d8a73935b05350983551f
Avi Chhetri <Avi Chhetri@DESKTOP-1KGM680>
2024-06-15 12:02:43 -0400

    get Alphanumeric String

--- EncryptDecrypt/Program.cs
+++ EncryptDecrypt/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace EncryptDecrypt
+﻿using Helper;
+
+namespace EncryptDecrypt
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("asdasd");
+            Console.WriteLine("Get Alphanumeric String of size {0} is {1}", 10, AlphaNumericStringHelper.GetAlphaNumericString(10));
         }
     }
 }
