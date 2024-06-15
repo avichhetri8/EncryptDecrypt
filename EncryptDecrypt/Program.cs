27d0b62e5e7b6a6018ff580eccb2c38fc843df84
Avi Chhetri <Avi Chhetri@DESKTOP-1KGM680>
2024-06-15 12:00:27 -0400

    output shown

---
+++ EncryptDecrypt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EncryptDecrypt
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("asdasd");
+        }
+    }
+}
